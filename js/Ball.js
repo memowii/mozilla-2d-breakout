9dab4c48e09b16038584569802ae757ec2c0e026
Guillermo García López <memowii@hotmail.com>
2017-01-09 20:24:19 -0600

    Create game2.js and run it

--- js/Ball.js
+++ js/Ball.js
@@ -1,14 +1,21 @@
-function Ball(x, y, ballRadius, color) {
+function Ball(ctx, x, y, ballRadius, color, ballSpeed) {
+    this.ctx = ctx;
     this.x = x;
     this.y = y;
     this.ballRadius = ballRadius;
     this.color = color;
+    this.ballSpeed = ballSpeed;
     
     this.draw = function () {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.closePath();
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+        this.ctx.closePath();
+    };
+    
+    this.changePos = function () {
+        this.x += this.ballSpeed;
+        this.y += -this.ballSpeed;
     };
 }
---
+++ js/game2.js
@@ -0,0 +1,21 @@
+function Game(canvas, ctx, ball) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.ball = ball;
+
+    this.draw = function() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ball.draw();
+        this.ball.changePos();
+
+        requestAnimationFrame(this.draw.bind(this));
+    };
+};
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var ball = new Ball(ctx, canvas.width/2, canvas.height-30, 10, "#0095DD", 2);
+var game = new Game(canvas, ctx, ball);
+
+game.draw();
