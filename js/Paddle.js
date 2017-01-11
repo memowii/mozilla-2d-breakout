a1284ebe791168ef2458310126c00d424094bc23
Guillermo García López <memowii@hotmail.com>
2017-01-10 20:15:17 -0600

    Move the Paddle with the keyboard

--- js/Paddle.js
+++ js/Paddle.js
@@ -4,6 +4,8 @@ function Paddle(x, y, width, height, color) {
     this.width = width;
     this.height = height;
     this.color = color;
+    this.rightPressed = false;
+    this.leftPressed = false;
 
     this.draw = function () {
         ctx.beginPath();
@@ -12,4 +14,32 @@ function Paddle(x, y, width, height, color) {
         ctx.fill();
         ctx.closePath();
     };
+
+    this.checkPressedButton = function () {
+        if(this.rightPressed && this.x < canvas.width - this.width) {
+            this.x += 7;
+        }
+        else if(this.leftPressed && this.x > 0) {
+            this.x -= 7;
+        }
+    };
+
+    this.keyDownHandler = function (e) {
+        if(e.keyCode == 39) {
+            this.rightPressed = true;
+        } else if(e.keyCode == 37) {
+            this.leftPressed = true;
+        }
+    };
+
+    this.keyUpHandler = function (e) {
+        if(e.keyCode == 39) {
+            this.rightPressed = false;
+        } else if(e.keyCode == 37) {
+            this.leftPressed = false;
+        }
+    };
+
+    document.addEventListener("keydown", this.keyDownHandler.bind(this), false);
+    document.addEventListener("keyup", this.keyUpHandler.bind(this), false);
 }
--- js/game2.js
+++ js/game2.js
@@ -8,6 +8,7 @@ function Game(ball, paddle) {
         this.ball.checkCollision();
         this.ball.changePosition();
         this.paddle.draw();
+        this.paddle.checkPressedButton();
 
         requestAnimationFrame(this.draw.bind(this));
     };
@@ -17,7 +18,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var ball = new Ball(canvas.width/2, canvas.height-30, 10, "#0095DD", 2);
-var padde = new Paddle((canvas.width-75)/2, canvas.height-10, 75, 10, "#0095DD");
-var game = new Game(ball, padde);
+var paddle = new Paddle((canvas.width-75)/2, canvas.height-10, 75, 10, "#0095DD");
+var game = new Game(ball, paddle);
 
 game.draw();
