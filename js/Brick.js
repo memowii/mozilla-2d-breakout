d232026f8fbadceb5707140015e710dc4ebb9e11
Guillermo García López <memowii@hotmail.com>
2017-01-12 12:04:56 -0600

    Creat Brick class

---
+++ js/Brick.js
@@ -0,0 +1,16 @@
+function Brick(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.status = 1;
+
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
